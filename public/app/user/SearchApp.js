39aca247b41437c6a5e3530cf1f7adc236f45c8a
thanathas333 <thanathas_333@hotmail.com>
2015-03-11 17:26:24 +0700

    sam14

---
+++ public/app/user/SearchApp.js
@@ -0,0 +1,23 @@
+/**
+ * Created by ZAM on 3/11/2015.
+ */
+
+var app = angular.module("SearchApp",[]);
+app.controller('SearchCtrl',function($scope,$http){
+    console.log("SearchCtrl...");
+
+    $scope.search = {
+    };
+
+    $scope.submitForm = function(){
+        console.log("submit...");
+
+        $http({
+            url : "/search",
+            method : 'post',
+            data : $scope.search
+        } ).success(function(response){
+            $scope.search = response;
+        })
+    }
+});
