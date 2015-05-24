805d32490fc31aeb49420167968196057c416f34
chickenlittle36 <on_sky36@hotmail.co.th>
2015-05-24 18:25:45 +0700

    kaii34

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -95,21 +95,10 @@ public class Application extends Controller {
         return ok(statusaddword.render());
     }
 
-    public static Result terminologystatus() {
-        return ok(terminologystatus.render());
-    }
-
     public static Result useraddform() {
         return ok(useraddform.render());
     }
 
-    public static Result terminologycom() {
-        return ok(terminologycom.render());
-    }
-
-    public static Result comment() {
-        return ok(comment.render());
-    }
 }
 
 
